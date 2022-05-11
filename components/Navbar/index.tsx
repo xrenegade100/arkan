ae447db3c54ca0397a57ba8ec70dcea94169e4b6
antgioia <antgioia10@gmail.com>
2022-05-11 12:04:37 +0200

    feat: add a lock's functionality for navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -8,9 +8,10 @@ import LogoIcon from '../LogoIcon';
 type Props = {
   onMenuClick?: () => any;
   hambugerIcon?: string;
+  isLock?: boolean;
 };
 
-const Navbar = ({ onMenuClick, hambugerIcon }: Props) => {
+const Navbar = ({ onMenuClick, hambugerIcon, isLock }: Props) => {
   const [path, setPath] = useState('');
   const router = useRouter();
 
@@ -20,41 +21,89 @@ const Navbar = ({ onMenuClick, hambugerIcon }: Props) => {
 
   return (
     <nav className='flex h-14 w-screen bg-primary-dark items-center justify-between'>
-      <button
-        onClick={onMenuClick}
-        className='material-icons pr-32 text-white w-60'
-      >
-        {hambugerIcon}
-      </button>
-      <div className='w-60 flex flex-1 justify-center'>
-        <Link href='/' passHref>
-          <LogoIcon size='base' />
-        </Link>
-      </div>
-      <div className='flex items-center text-white w-60'>
-        <Link href='/segnala'>
-          <button
-            className={clsx('p-4 hover:bg-secondary-main font-bold uppercase', {
-              'bg-secondary-main': path === '/segnala',
-            })}
-          >
-            Segnala
-          </button>
-        </Link>
-        <Link href='/feed'>
-          <button
-            className={clsx(
-              'p-4 hover:bg-secondary-main font-bold uppercase mr-4',
-              {
-                'bg-secondary-main': path === '/feed',
-              }
-            )}
-          >
-            Feed
-          </button>
-        </Link>
-        <Avatar color='#333' name='Giovanni' />
-      </div>
+      {!isLock && (
+        <button
+          onClick={onMenuClick}
+          className='material-icons pr-32 text-white w-60'
+        >
+          {hambugerIcon}
+        </button>
+      )}
+
+      {isLock && <div className='w-60'></div>}
+
+      {!isLock && (
+        <div className='w-60 flex flex-1 justify-center'>
+          <Link href='/' passHref>
+            <LogoIcon size='base' />
+          </Link>
+        </div>
+      )}
+
+      {isLock && (
+        <div className='w-60 flex flex-1 justify-center'>
+          <Link href='/' passHref>
+            <LogoIcon size='base' />
+          </Link>
+        </div>
+      )}
+      {!isLock && (
+        <div className='flex items-center text-white w-60'>
+          <Link href='/segnala'>
+            <button
+              className={clsx(
+                'p-4 hover:bg-secondary-main font-bold uppercase',
+                {
+                  'bg-secondary-main': path === '/segnala',
+                }
+              )}
+            >
+              Segnala
+            </button>
+          </Link>
+          <Link href='/feed'>
+            <button
+              className={clsx(
+                'p-4 hover:bg-secondary-main font-bold uppercase mr-4',
+                {
+                  'bg-secondary-main': path === '/feed',
+                }
+              )}
+            >
+              Feed
+            </button>
+          </Link>
+          <Avatar color='#333' name='Giovanni' />
+        </div>
+      )}
+      {isLock && (
+        <div className='flex items-center text-white w-60'>
+          <Link href='/login'>
+            <button
+              className={clsx(
+                'p-4 hover:bg-secondary-main font-bold uppercase',
+                {
+                  'bg-secondary-main': path === '/login',
+                }
+              )}
+            >
+              Login
+            </button>
+          </Link>
+          <Link href='/registrazione'>
+            <button
+              className={clsx(
+                'p-4 hover:bg-secondary-main font-bold uppercase mr-4',
+                {
+                  'bg-secondary-main': path === '/registrazione',
+                }
+              )}
+            >
+              Registrazione
+            </button>
+          </Link>
+        </div>
+      )}
     </nav>
   );
 };
