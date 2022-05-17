f226f6af9d048ec8ce1fc0f88049a903fe63ab20
antgioia <antgioia10@gmail.com>
2022-05-17 18:27:22 +0200

    feat: add image of homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ const Home: NextPage = () => {
   switch (modal) {
     case 1: {
       title = 'Nagging';
-      src = '/nagging.png';
+      src = '/nagging.jpg';
       text =
         'I dark pattern appartenenti alla categoria Nagging si manifestano durante la normale interazione dell’utente come delle ripetute intrusioni. L’utente che è intenzionato a svolgere un task viene interrotto una o più volte da altri task non direttamente collegati al suo intento originale. La presenza di Nagging si può manifestare con pop-up che oscurano l’interfaccia, pubblicità improvvise a schermo intero causando disturbo, oppure qualunque azione che ostruisce o sposta il focus dell’utente. Nell’esempio riportato , nell’app Instagram un pop-up per attivare le notifiche interrompe il task che sta svolgendo l’utente, cioè scorrere il proprio feed. All’interno del pop-up non abbiamo un’ opzione per dire “No”, ma solo “Non adesso” e quindi il pop-up in seguito verrà mostrato nuovamente.';
       break;
@@ -44,7 +44,7 @@ const Home: NextPage = () => {
     }
     case 3: {
       title = 'Price Comparison Prevention';
-      src = '/pricecomparisonprevention.png';
+      src = '/pricecomparisonprevention.jpg';
       text =
         'Con la presenza di questo pattern viene reso difficile il confronto del prezzo di un prodotto, o servizio, rispetto ad altri, in modo da impedire all’utente di informarsi per prendere una decisione. Un’istanza di questo pattern è stata rilevata nel sito “sainsburys.co.uk” dove non è possibile confrontare il primo articolo (mele singole), espresso con un prezzo unitario, con il secondo articolo (box di mele di cui non conosciamo il peso), espresso con un prezzo al kilogrammo, motivo per cui non possiamo valutare qual’è l’articolo conveniente da acquistare';
       break;
@@ -58,7 +58,7 @@ const Home: NextPage = () => {
     }
     case 5: {
       title = 'Forced Continuity';
-      src = '/forcedcontinuity.png';
+      src = '/forcedcontinuity.jpg';
       text =
         'Il costo di un servizio richiesto dall’utente continua ad essergli addebitato anche dopo la sua scadenza. Si verifica, quindi, quando termina la prova gratuita di un servizio, o quando la scadenza di esso viene raggiunta. L’abbonamento viene rinnovato automaticamente senza preavviso, addebitando il costo all’utente.';
       break;
@@ -72,49 +72,49 @@ const Home: NextPage = () => {
     }
     case 7: {
       title = 'Sneak into Basket';
-      src = '/sneakintobasket.png';
+      src = '/sneakintobasket.jpg';
       text =
         'Il pattern Sneak into Basket aggiunge prodotti non scelti dall’utente al suo carrello online. L’inserimento di nuovi prodotti è spesso giustificato come dei suggerimenti basati su altri articoli acquistati. Ciò può indurre l’utente ad acquistare involontariamente questi articoli, a meno che non se ne accorga prima del check-out. Un esempio di questo comportamento è possibile trovarlo sul sito sportdirect.com in cui al momento dell’inserimento di un prodotto al carrello viene inserita automaticamente anche una rivista in modo da ingannare l’utente.';
       break;
     }
     case 8: {
       title = 'Bait and Switch';
-      src = '/baitandswitch.png';
+      src = '/baitandswitch.jpg';
       text =
         'L’utente si propone di fare una cosa, ma invece accade una cosa diversa e indesiderabile: un pulsante “X” rosso esegue un’operazione/azione diversa dalla chiusura di una finestra pop-up. Questo dark pattern è stato impiegato da Microsoft durante la campagna di aggiornamento di Windows 10: la finestra permette di effettuare l’aggiornamento, ma la classica X in alto a destra invece che chiudere la finestra e non effettuare l’aggiornamento, dà inizio al processo.';
       break;
     }
     case 9: {
       title = 'Social Pyramid';
-      src = '/socialpyramid.png';
+      src = '/socialpyramid.jpg';
       text =
         'Questo pattern forza un utente ad invitare altri utenti per accedere al servizio. È un modello usato soprattutto in piattaforme di social media e giochi online, in cui l’utente viene premiato con potenziamenti (o benefici) se invita altri utenti ad utilizzare la piattaforma. Rilevato nell’app FarmVille: il gioco suggerisce di invitare amici, per ottenere accesso a funzionalità che non sono accessibili se si gioca da soli';
       break;
     }
     case 10: {
       title = 'Privacy Zuckering';
-      src = '/privacyzuckering.png';
+      src = '/privacyzuckering.jpg';
       text =
         'L’obiettivo è quello di ingannare l’utente in modo da fargli condividere più informazioni personali di quante ne voglia fornire. Molto spesso è utilizzato per vendere le informazioni degli utenti ad aziende di terze parti, includendo una clausola nei termini e condizioni o privacy policy dei siti web. Per esempio, questo pattern è stato rilevato nell’app mobile Messenger: appena installata l’app viene chiesto il permesso all’utente di accedere alla sua rubrica per creare una rete di amici; in questo modo l’azienda può utilizzare questa rete creata, per mostrare annunci pertinenti agli interessi dell’utente e dei suoi amici. Tuttavia il modo in cui questo viene fatto è ingannevole, dal momento che, l’utente per evitare l’accesso alla rubrica da parte dell’app, deve, in ordine, cliccare su “Learn More”, leggere l’intera privacy policy, e infine negare il consenso.';
       break;
     }
     case 11: {
       title = 'Gamification';
-      src = '/gamification.png';
+      src = '/gamification.jpg';
       text =
         'Si vanno ad indicare tutte quelle circostanze all’interno di un servizio in cui alcuni aspetti possono essere "guadagnati" soltanto attraverso lo svolgimento ripetuto di determinate azioni. Nella Figura, vediamo come il sito turntable.fm cerca di convincere gli utenti a usarlo per più tempo, in modo da far guadagnare più punti esperienza a chi è più popolare o lo utilizza maggiormente, andando a discapito degli utenti occasionali.';
       break;
     }
     case 12: {
       title = 'Hidden Information';
-      src = '/hiddeninformation.png';
+      src = '/hiddeninformation.jpg';
       text =
         'Si tratta di informazioni che non sono rese note all’utente attraverso contenuti nascosti, testo scolorito o di piccole dimensioni. L’obiettivo di questo dark pattern è quello di far passare informazioni rilevanti come irrilevanti. Questa tecnica viene utilizzata dal sito Greenpeace, in particolare nelle email di newsletter l’opzione per disiscriversi dalla mailing list non è evidenziata come se fosse un link, bensì come un testo normale.';
       break;
     }
     case 13: {
       title = 'Preselection';
-      src = '/preselection.png';
+      src = '/preselection.jpg';
       text =
         'Una situazione in cui un’opzione è già selezionata di default nella pagina. Di solito si manifesta come una scelta che il proprietario del sito vuole che l’utente compia. Molto spesso però è utilizzata per scopi che vanno contro gli interessi dell’utente, o che causano effetti indesiderati. Un utilizzo è stato rilevato sul sito next.co.uk, che al momento della registrazione, preseleziona un radio button per ricevere la prima copia della loro brochure gratuitamente, ma leggendo i termini di utilizzo si scopre che dopo il primo invio ce ne saranno altri 3 a pagamento (3.75£ ognuno), i cui costi saranno addebitati direttamente sulla carta di credito inserita dall’utente.';
       break;
@@ -128,28 +128,28 @@ const Home: NextPage = () => {
     }
     case 15: {
       title = 'Toying with Emotions';
-      src = '/toyingwithemotions.png';
+      src = '/toyingwithemotions.jpg';
       text =
         'Consiste nel cercare di far provare determinate emozioni all’utente mediante l’utilizzo di uno o più aspetti dell’interfaccia, come testo, colori, immagini. L’esempio in figura è tratto da un’email inviata da “Trump Headquarters” contente un sondaggio di parte, che cerca di far provare emozioni negative se non si vuole selezionare la prima opzione (“Sto con il presidente Trump), poiché il testo della seconda opzione è “Credo ai democratici e alle fake news”.';
       break;
     }
     case 16: {
       title = 'False Hierarchy';
-      src = '/falsehierarchy.png';
+      src = '/falsehierarchy.jpg';
       text =
         'È un dark pattern con il quale si cerca di convincere l’utente ad effettuare una scelta piuttosto che un’altra rendendola più appetibile con maggiori decorazioni o più interattività. In particolare si cerca di dare l’impressione che l’opzione evidenziata sia l’unica o la migliore, come sul sito reddit.com.';
       break;
     }
     case 17: {
       title = 'Disguised Ad';
-      src = '/disguisedad.png';
+      src = '/disguisedad.jpg';
       text =
         'Un tipo di design pattern che implica l’utilizzo di giochi , pulsanti o altri elementi interattivi che l’utente trova in una pagina, che mascherano in realtà una pubblicità. In casi estremi l’intera pagina web è trasformata in un annuncio, facendo sì che l’utente in qualsiasi punto clicchi viene ridirezionato verso un’altra pagina. In questo esempio tratto dal sito softpedia.com viene mostrato un bottone di “Download Now” che in realtà rimanda ad un sito esterno, invece che effettuare il download del programma richiesto.';
       break;
     }
     case 18: {
       title = 'Trick Questions';
-      src = '/trickquestions.png';
+      src = '/trickquestions.jpg';
       text =
         'Questo tipo di dark pattern comprende frasi formulate volontariamente in maniera confusa, con doppie negazioni, o che sembrano indicare una cosa, ma in realtà significano tutt’altro. Con questo pattern si utilizza il linguaggio in maniera tale da manipolare le intenzioni dell’utente. Nella Figura si può vedere come sul sito codemasters.com viene utilizzata una doppia negazione correlata a questa checkbox per confondere l’utente ed indurlo ad iscriversi a una newsletter. Da notare la formulazione volutamente confusionaria “Non deselezionare questa casella se vuoi essere contattato tramite email...”';
       break;
@@ -214,7 +214,7 @@ const Home: NextPage = () => {
                 Tassonomia
               </span>
             </div>
-            <div className='w-full bg-gray-200 rounded-b-3xl px-3'>
+            <div className='w-full bg-gray-200 rounded-b-3xl'>
               <Accordion allowToggle>
                 <AccordionItem>
                   <h2>
@@ -302,13 +302,26 @@ const Home: NextPage = () => {
                     alcune.
                   </AccordionPanel>
                 </AccordionItem>
-                <AccordionItem>
+                <AccordionItem
+                  borderRadius={'3xl'}
+                  __css={{ borderBottomRadius: '3xl' }}
+                  _hover={{
+                    borderBottomRadius: '3xl',
+                    opacity: '0.91',
+                    textColor: 'black',
+                  }}
+                >
                   <h2>
-                    <AccordionButton>
+                    <AccordionButton borderBottomRadius={'1.5rem'}>
                       <Box
                         flex='1'
                         textAlign='left'
                         className='text-base font-bold'
+                        _hover={{
+                          borderBottomRadius: '3xl',
+                          opacity: '0.91',
+                          textColor: 'black',
+                        }}
                       >
                         Forced Action
                       </Box>
@@ -343,6 +356,10 @@ const Home: NextPage = () => {
                   fontWeight={'extrabold'}
                   transition='all 0.2s'
                   as={Button}
+                  onClick={() => {
+                    setModal(1);
+                    onOpen();
+                  }}
                 >
                   Nagging
                 </MenuButton>
@@ -365,6 +382,7 @@ const Home: NextPage = () => {
                 <MenuList bgColor={'gray.700'}>
                   <MenuItem
                     _hover={{ bg: '#FFD95A', opacity: '90%' }}
+                    _focus={{ bg: '#FFD95A', opacity: '90%' }}
                     textColor={'white'}
                     onClick={() => {
                       onOpen();
@@ -375,6 +393,7 @@ const Home: NextPage = () => {
                   </MenuItem>
                   <MenuItem
                     _hover={{ bg: '#FFD95A', opacity: '90%' }}
+                    _focus={{ bg: '#FFD95A', opacity: '90%' }}
                     textColor={'white'}
                     onClick={() => {
                       onOpen();
@@ -385,6 +404,7 @@ const Home: NextPage = () => {
                   </MenuItem>
                   <MenuItem
                     _hover={{ bg: '#FFD95A', opacity: '90%' }}
+                    _focus={{ bg: '#FFD95A', opacity: '90%' }}
                     textColor={'white'}
                     onClick={() => {
                       onOpen();
@@ -413,6 +433,7 @@ const Home: NextPage = () => {
                 <MenuList bgColor={'gray.700'} defaultChecked>
                   <MenuItem
                     _hover={{ bg: '#FFD95A', opacity: '90%' }}
+                    _focus={{ bg: '#FFD95A', opacity: '90%' }}
                     textColor={'white'}
                     onClick={() => {
                       onOpen();
@@ -423,6 +444,7 @@ const Home: NextPage = () => {
                   </MenuItem>
                   <MenuItem
                     _hover={{ bg: '#FFD95A', opacity: '90%' }}
+                    _focus={{ bg: '#FFD95A', opacity: '90%' }}
                     textColor={'white'}
                     onClick={() => {
                       onOpen();
@@ -433,6 +455,7 @@ const Home: NextPage = () => {
                   </MenuItem>
                   <MenuItem
                     _hover={{ bg: '#FFD95A', opacity: '90%' }}
+                    _focus={{ bg: '#FFD95A', opacity: '90%' }}
                     textColor={'white'}
                     onClick={() => {
                       onOpen();
@@ -443,6 +466,7 @@ const Home: NextPage = () => {
                   </MenuItem>
                   <MenuItem
                     _hover={{ bg: '#FFD95A', opacity: '90%' }}
+                    _focus={{ bg: '#FFD95A', opacity: '90%' }}
                     textColor={'white'}
                     onClick={() => {
                       onOpen();
@@ -471,6 +495,7 @@ const Home: NextPage = () => {
                 <MenuList bgColor={'gray.700'}>
                   <MenuItem
                     _hover={{ bg: '#FFD95A', opacity: '90%' }}
+                    _focus={{ bg: '#FFD95A', opacity: '90%' }}
                     textColor={'white'}
                     onClick={() => {
                       onOpen();
@@ -481,6 +506,7 @@ const Home: NextPage = () => {
                   </MenuItem>
                   <MenuItem
                     _hover={{ bg: '#FFD95A', opacity: '90%' }}
+                    _focus={{ bg: '#FFD95A', opacity: '90%' }}
                     textColor={'white'}
                     onClick={() => {
                       onOpen();
@@ -491,8 +517,8 @@ const Home: NextPage = () => {
                   </MenuItem>
                   <MenuItem
                     _hover={{ bg: '#FFD95A', opacity: '90%' }}
+                    _focus={{ bg: '#FFD95A', opacity: '90%' }}
                     textColor={'white'}
-                    onClick={() => alert('Kagebunshin')}
                     onClick={() => {
                       onOpen();
                       setModal(11);
@@ -522,6 +548,7 @@ const Home: NextPage = () => {
                 <MenuList bgColor={'gray.700'}>
                   <MenuItem
                     _hover={{ bg: '#FFD95A', opacity: '90%' }}
+                    _focus={{ bg: '#FFD95A', opacity: '90%' }}
                     textColor={'white'}
                     onClick={() => {
                       onOpen();
@@ -532,6 +559,7 @@ const Home: NextPage = () => {
                   </MenuItem>
                   <MenuItem
                     _hover={{ bg: '#FFD95A', opacity: '90%' }}
+                    _focus={{ bg: '#FFD95A', opacity: '90%' }}
                     textColor={'white'}
                     onClick={() => {
                       onOpen();
@@ -542,6 +570,7 @@ const Home: NextPage = () => {
                   </MenuItem>
                   <MenuItem
                     _hover={{ bg: '#FFD95A', opacity: '90%' }}
+                    _focus={{ bg: '#FFD95A', opacity: '90%' }}
                     textColor={'white'}
                     onClick={() => {
                       onOpen();
@@ -552,6 +581,7 @@ const Home: NextPage = () => {
                   </MenuItem>
                   <MenuItem
                     _hover={{ bg: '#FFD95A', opacity: '90%' }}
+                    _focus={{ bg: '#FFD95A', opacity: '90%' }}
                     textColor={'white'}
                     onClick={() => {
                       onOpen();
@@ -562,6 +592,7 @@ const Home: NextPage = () => {
                   </MenuItem>
                   <MenuItem
                     _hover={{ bg: '#FFD95A', opacity: '90%' }}
+                    _focus={{ bg: '#FFD95A', opacity: '90%' }}
                     textColor={'white'}
                     onClick={() => {
                       onOpen();
@@ -572,6 +603,7 @@ const Home: NextPage = () => {
                   </MenuItem>
                   <MenuItem
                     _hover={{ bg: '#FFD95A', opacity: '90%' }}
+                    _focus={{ bg: '#FFD95A', opacity: '90%' }}
                     textColor={'white'}
                     onClick={() => {
                       onOpen();
@@ -582,6 +614,7 @@ const Home: NextPage = () => {
                   </MenuItem>
                   <MenuItem
                     _hover={{ bg: '#FFD95A', opacity: '90%' }}
+                    _focus={{ bg: '#FFD95A', opacity: '90%' }}
                     textColor={'white'}
                     onClick={() => {
                       onOpen();
