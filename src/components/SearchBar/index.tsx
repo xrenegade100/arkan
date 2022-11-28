780952bdb216eec03934e914ad2811d0082c1f3b
Giovanni <scorziello.giovanni00@gmail.com>
2022-11-28 14:01:46 +0100

    style: responsive dimension

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const SearchBar: React.FC<Props> = ({ value, onChange }: Props) => (
   <div className="w-full h-screen flex justify-center items-center">
-    <div className="w-4/5 md:w-2/3 lg:w-2/4 xl:w-1/3 p-1 rounded-full bg-gradient-to-r from-primary-main  first-letter:   b to-secondary-main shadow-lg">
+    <div className="w-4/5 md:w-3/4 lg:w-2/3 xl:w-2/4 p-1 rounded-full bg-gradient-to-r from-primary-main  first-letter:   b to-secondary-main shadow-lg">
       <div className="w-full h-auto p-1 rounded-full bg-white flex justify-center items-center">
         <input
           type="text"
