f06cc7b14ea47cc27711e3711f468d1eab9734d0
Giovanni <scorziello.giovanni00@gmail.com>
2022-12-26 00:59:46 +0100

    feat: internal layout

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import UserCard from './UserCard'
 
-const Sidebar: React.FC = () => {
+interface Props {
+  username: string;
+  email: string;
+  children: React.ReactElement | React.ReactElement[];
+}
+
+const Sidebar: React.FC<Props> = ({username, email, children}: Props) => {
   return (
     <div className="w-full sm:w-2/4 md:w-4/12 lg:w-1/5 fixed top-14 left-0 bottom-0 bg-primary-main sm:rounded-br-quarter transition-all">
-      <UserCard email='scorziello.giovanni00@gmail.com' username='BraindeadHermit'/>
+      <div className='flex flex-col w-full justify-start items-center py-6'>
+        <UserCard email={email} username={username}/>
+        <div className='py-6 w-full flex flex-col justify-center items-end'>
+          {children}
+        </div>
+      </div>
     </div>
   );
 };
