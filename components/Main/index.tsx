231a373c48285ab95fe3bb11418ead684b12ce6e
Giovanni <scorziello.giovanni00@gmail.com>
2022-12-29 14:17:34 +0100

    fix: email truncate when text to long and bg colot

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -10,7 +10,9 @@ const Main: React.FC<Props> = ({ children }: Props) => {
   return (
     <div className="w-full h-screen m-0 p-0">
       <Navbar />
-      <Sidebar />
+      <Sidebar username='BraindeadHermit' email='scorziello.giovanni00@gmail.com'>
+        <div></div>
+      </Sidebar>
       <div className="w-full h-full m-0 p-0">{children}</div>
     </div>
   );
