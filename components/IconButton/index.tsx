973df6bdc69b6e8039dfe4c4d84ed5aea137a5d7
Giovanni <scorziello.giovanni00@gmail.com>
2023-01-10 14:24:20 +0100

    feat: show text on hover

--- components/IconButton/index.tsx
+++ components/IconButton/index.tsx
@@ -1,11 +1,12 @@
 import clsx from "clsx";
-import React from "react";
+import React, { useRef, useEffect } from "react";
 
 interface Props {
     icon: string;
     text?: string;
     variant?: 'primary' | 'secondary';
     size?: 'sm' | 'base' | 'lg';
+    showTextOnHover?: boolean;
 }
 
 const IconButton: React.FC<Props> = ({
@@ -13,13 +14,32 @@ const IconButton: React.FC<Props> = ({
   text,
   variant,
   size,
+  showTextOnHover,
 }: Props) => {
+  const span = useRef<HTMLSpanElement>(null);
+
+  if(text === '')
+    showTextOnHover = false;
+
   return (
-    <button className={clsx("rounded-full p-2 shadow-md transition-colors ease-in",
+    <button className={clsx("rounded-full p-2 shadow-md transition-all ease-in",
     {
       "bg-secondary-main hover:bg-secondary-accent": variant === 'secondary',
       "bg-primary-main hover:bg-primary-accent": variant === 'primary',
-    })}>
+    })}
+
+    onMouseEnter={() => {
+      if(showTextOnHover === true) {
+        span?.current?.classList.add("w-auto", "mx-1", "visible");
+        span?.current?.classList.remove("w-0", "mx-0", "hidden");
+      }
+    }}
+    onMouseLeave={() => {
+      if(showTextOnHover === true) {
+        span?.current?.classList.add("w-0", "mx-0", "hidden");
+        span?.current?.classList.remove("w-auto", "mx-1", "visible");
+      }
+    }}>
       <div className="flex justify-center items-center">
         <span className={clsx("material-symbols-rounded font-bold text-white",
           {
@@ -28,7 +48,11 @@ const IconButton: React.FC<Props> = ({
             "md-32": size === 'lg',
           }
         )}>{icon}</span>
-        { text && <span className={clsx("mx-1 font-bold font-body text-white uppercase",
+        { text && <span ref={span} className={clsx("font-bold font-body text-white uppercase transition-width",
+          {
+            'w-0 mx-0 hidden': showTextOnHover,
+            "mx-1": !showTextOnHover,
+          },
           {
             "text-base": size === 'base',
             "text-xs": size === 'sm',
@@ -43,6 +67,7 @@ IconButton.defaultProps = {
     text: '',
     variant: 'secondary',
     size: 'base',
+    showTextOnHover: false,
 }
 
 export default IconButton;
