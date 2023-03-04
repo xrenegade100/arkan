6ef62ae4bf3547c6675ac8210a036a66d1062926
Giovanni <scorziello.giovanni00@gmail.com>
2023-03-04 12:39:36 +0100

    feat: user detected DP card layout

---
+++ components/DarkPatternCard/index.tsx
@@ -0,0 +1,73 @@
+import clsx from "clsx";
+import Image from "next/image";
+import React from "react";
+import Avatar from "../Avatar";
+import IconButton from "../IconButton";
+import Toast from "../Toast";
+
+interface Props {
+  username: string,
+  siteName: string,
+  siteLink: string,
+  description: string,
+  dangerLevel: number,
+  darkPatternType: string,
+  date: string,
+  imageLink: string,
+  detectionMode: 'rilevato tramite segnalazione' | 'rilevato tramite analisi',
+
+}
+
+const UserDetectedCard: React.FC<Props> = ({
+  username,
+  siteName,
+  siteLink,
+  description,
+  dangerLevel,
+  darkPatternType,
+  date,
+  imageLink,
+  detectionMode,
+}: Props) => {
+  return (
+    <div className="w-11/12 lg:w-2/5 rounded-md shadow-md bg-opacity-50 bg-primary-accent">
+      <div className="w-full flex flex-col-reverse justify-between items-start md:flex-row md:justify-between md:items-center p-4">
+        <div className="w-1/2 flex justify-start items-center mt-2 md:mt-0">
+          <Avatar dimen="xs" imageUrl="https://st3.depositphotos.com/9998432/13335/v/600/depositphotos_133352010-stock-illustration-default-placeholder-man-and-woman.jpg"/>
+          <span className="ml-2 md:ml-4  font-body text-lg md:text-xl font-bold">{username}</span>
+        </div>
+        <Toast text={detectionMode.toUpperCase()} className="uppercase" onClick={() => {}} />
+      </div>
+      <div className="relative w-full">
+        <img src={imageLink} className="w-full object-cover" height={100} alt={darkPatternType}/>
+        <Toast text={darkPatternType.toUpperCase()} className="absolute bottom-0 right-0 m-4 uppercase" onClick={() => {}}/>
+      </div>
+      <div className="mx-4 my-1 md:my-2 flex justify-start items-center">
+        <span className={clsx("w-6 h-6 rounded-full",
+        {
+          'bg-green-600': dangerLevel === 1,
+          'bg-yellow-600': dangerLevel >= 2 && dangerLevel <= 3,
+          'bg-red-600': dangerLevel >= 4,
+        })}
+        title={`livello di pericolo: ${dangerLevel}`}
+        />
+        <a href={siteLink} className="ml-1 md:ml-2 font-body text-xl md:text-2xl font-bold">{siteName}</a>
+      </div>
+      <div className="mx-4">
+        <span className="font-body">
+          {description}
+        </span>
+      </div>
+      <div className="py-1 md:py-2 w-full flex justify-between items-end">
+        <div className="flex justify-start items-center ml-4">
+          <IconButton icon="favorite" className="mr-1 md:mr-2"/>
+          <IconButton icon="share" className="mr-1 md:mr-2"/>
+          <IconButton icon="add_comment"/>
+        </div>
+        <span className="mr-4 text-sm font-light font-body">{date}</span>
+      </div>
+    </div>
+  )
+}
+
+export default UserDetectedCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Main from '../components/Main';
 import Head from 'next/head';
+import DarkPatternCard from '../components/DarkPatternCard';
 
 const Home: NextPage = () => {
   return (
@@ -12,7 +13,18 @@ const Home: NextPage = () => {
       </Head>
 
       <Main>
-        <p>ciao</p>
+        <br/><br/><br/><br/><br/>
+        <DarkPatternCard 
+          username='BraindeadHermit'
+          detectionMode='rilevato tramite analisi'
+          dangerLevel={3}
+          siteName='Facebook'
+          siteLink='https://www.facebook.com/'
+          date={new Date().toLocaleDateString()}
+          darkPatternType="nagging"
+          description='very very long description about this dark pattern'
+          imageLink='https://assets.puzzlefactory.pl/puzzle/297/747/original.jpg'
+        />
       </Main>
     </div>
   );
