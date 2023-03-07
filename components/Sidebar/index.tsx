c0d6dcfc7283b7cdb7a216c158d8b0bcb261c1f9
Giovanni <scorziello.giovanni00@gmail.com>
2023-03-07 18:49:24 +0100

    feat: create home header

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 const Sidebar: React.FC<Props> = ({ username, email, children, isVisible}: Props) => {
   return (
-    <div className={clsx("z-10 fixed top-14 left-0 bottom-0 bg-primary-main sm:rounded-br-quarter transition-all",
+    <div className={clsx("z-10 fixed top-14 left-0 bottom-0 bg-primary-main transition-all",
       {
         "w-full sm:w-2/4 md:w-4/12 lg:w-1/5": isVisible === true,
       },
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,30 @@
 import type { NextPage } from 'next';
-import Footer from '../components/Footer';
+import { useRef, useState, Ref } from 'react';
+import SearchBar from '../components/SearchBar';
 
 const Home: NextPage = () => {
+  const [link, setLink] = useState('');
+  const scrollPoint: Ref<HTMLDivElement> = useRef(null);
+
+  const handleClick = () => {
+    scrollPoint.current?.scrollIntoView({behavior: 'smooth'});
+  };
+
   return (
     <div>
-      <br/><br/><br/><br/><br/>
-      <Footer/>
+      <header className='relative w-full h-screen flex justify-center items-center bg-header bg-no-repeat bg-cover'>
+        <SearchBar value={link as string} onClick={() => {}} onChange={(e) => {
+          setLink(e.target.value);
+        }}/>
+        <div className='absolute bottom-0 left-0 w-full h-1/4 bg-gradient-to-t from-black to-transparent flex justify-center items-center'>
+          <button className='flex justify-center items-center' onClick={handleClick}>
+            <span className='text-3xl md:text-5xl lg:text-6xl font-body font-bold text-white text-center'>Cosa sono i <span className='text-primary-main'>Dark Pattern</span></span>
+            <span className='text-white material-symbols-rounded md-32 md:md-56 lg:md-64'>expand_more</span>
+          </button>
+        </div>
+      </header>
+      <div ref={scrollPoint} className="p-14">
+      </div>
     </div>
   );
 };
