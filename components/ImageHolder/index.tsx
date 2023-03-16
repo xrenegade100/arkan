099d2dcddfb480f641de7fb57cc192fadecaad32
Giovanni <scorziello.giovanni00@gmail.com>
2023-03-16 13:15:34 +0100

    fix: unused variableù

--- components/ImageHolder/index.tsx
+++ components/ImageHolder/index.tsx
@@ -4,14 +4,12 @@ import React, { useState, useEffect } from 'react';
 import IconButton from '../IconButton';
 
 interface Props {
-  index: number;
   name: string;
   imageToShow: File;
   onClick: () => void;
 }
 
 const ImageHolder: React.FC<Props> = ({
-  index,
   name,
   imageToShow,
   onClick,
