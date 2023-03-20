24d141fe270e7bde15c8fd04b9d13a85b3b6adc1
Giovanni <scorziello.giovanni00@gmail.com>
2023-03-20 17:51:06 +0100

    fix: access denied banner uiù

--- components/AccessDeniedBanner/index.tsx
+++ components/AccessDeniedBanner/index.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const AccessDeniedBanner = () => (
   <div className="flex flex-col justify-center items-center">
     <img src="./eye_denied.svg" />
-    <span className="text-gray-400 text-lg font-body font-semibold">
-      Non hai accesso a questi dati
+    <span className="text-gray-400 text-lg font-body font-semibold mt-4">
+      ERRORE DI ACCESSO
     </span>
   </div>
 );
